3eddb79de9f765ff845ad861373caf34419981ea
NaNRaligun <nanrailgun@aliyun.com>
2020-03-05 17:58:40 +0800

    init

---
+++ src/test/java/com/nanrailgun/springbootoss/SpringbootOssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanrailgun.springbootoss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
