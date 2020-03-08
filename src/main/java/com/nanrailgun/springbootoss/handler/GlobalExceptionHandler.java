68875d77615b97a7eca7ce71d945bd30195915de
“NaNrailgun <422780767@qq.com>
2020-03-08 23:40:07 +0800

    文件过大异常处理

---
+++ src/main/java/com/nanrailgun/springbootoss/handler/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.nanrailgun.springbootoss.handler;
+
+import com.nanrailgun.springbootoss.vo.ResultVo;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MultipartException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    /**
+     * 拦截文件过大异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(MultipartException.class)
+    public ResultVo SizeLimitExceededExceptionHandler(Exception e){
+        return ResultVo.builder().status_code(0).message("上传失败 文件过大").build();
+    }
+
+}
